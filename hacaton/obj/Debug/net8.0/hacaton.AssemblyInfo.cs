7d495e049a59c13013bb54b74b8e8edf369c5193
allahverdisultanov <allahverdisultanov674@gmail.com>
2024-12-06 11:07:39 +0400

    asdguc

--- hacaton/obj/Debug/net8.0/hacaton.AssemblyInfo.cs
+++ hacaton/obj/Debug/net8.0/hacaton.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("hacaton")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17939f82aed549be540a1138ab40a539fece2ffb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e0d39de9dd20571e418a7cffab7d7eb42ecc48c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("hacaton")]
 [assembly: System.Reflection.AssemblyTitleAttribute("hacaton")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
